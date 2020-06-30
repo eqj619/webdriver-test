93d91ea222a614e0e9f9726834002001feae003f
eqj619 <eqj619@gmail.com>
2020-06-29 18:08:54 -0700

    fix an issue for parse points

--- atpChecket.py
+++ atpChecket.py
@@ -6,6 +6,14 @@ from bs4 import BeautifulSoup
 import urllib.request as req
 import json
 import requests
+import re
+
+def cleanUpTag(listTag):
+    if type(listTag.string) != type(None):
+        return(listTag.string.replace("\n","").replace("\t", ""))
+    else:
+        return("")
+
 
 #===
 browser = webdriver.Chrome()
@@ -28,21 +36,74 @@ count = 0
 for tmp in rankdates:
         if type(tmp.string) != type(None):
             rankdate = tmp.string.replace("\n","").replace("\t", "")
-            if len(rankdate) == 10:
+            if len(rankdate) == 10: # reject other pulldown menu
                 if rankdate[0] == '1' or rankdate[0] == '2':
-                    if count != 0:
+                    if count != 0:  # skip default pulldown value
                         #print(rankdate.replace(".","-"))
                         rankDataList.append(rankdate.replace(".","-"))
                     count += 1
 
 
 # Retrieve ranking history at Japanese player
+listRank  =[]
+listMove = []
+listPlayer = []
+listAge = []
+listPoints = []
+listTourn = []
+listPts = []
+listNext = []
+
 for tag in rankDataList:
-    atpUrl = "https://www.atptour.com/en/rankings/singles?rankDate=" + tag + "&rankRange=1-5000&countryCode=JPN"
+    atpUrl = "https://www.atptour.com/en/rankings/singles?rankDate=" + tag + "&rankRange=1-1000&countryCode=JPN"
     browser.get(atpUrl)
     time.sleep(1)
 
+    pagehtml = browser.page_source
+    soup = BeautifulSoup(pagehtml, 'html.parser')
+    listRank = soup.find_all(class_ = 'rank-cell')
+    listMove = soup.find_all(class_ = 'move-cell')
+    listPlayer = soup.find_all(class_ = 'player-cell')
+    listAge = soup.find_all(class_ = 'age-cell')
+    listPoints = soup.find_all(class_ = 'points-cell')
+    listTourn = soup.find_all(class_ = 'tourn-cell')
+    listPts = soup.find_all(class_ = 'pts-cell')
+    listNext = soup.find_all(class_ = 'next-cell')
+
+    #for tmp in listPlayer:
+    #    print(tmp.find('a').string)
+
+    for i in range(len(listRank)):
+        s = cleanUpTag(listPlayer[i].find('a'))
+        ss = cleanUpTag(listPoints[i].find('a'))
+        sss = cleanUpTag(listTourn[i].find('a'))
+        print(tag + ", ",
+            cleanUpTag(listRank[i])+ ", ",
+            cleanUpTag(listMove[i]) + ", ",
+            re.sub(r"^\s+", "", s)  + ", ",
+            cleanUpTag(listAge[i])  + ", ",
+            ss + ", ",
+            sss + ", ",
+            cleanUpTag(listPts[i])  + ", ",
+            cleanUpTag(listNext[i])  + ", ",
+            )
+    #time.sleep(1)
+
+
 '''
+<a href="/en/players/kei-nishikori/n552/overview" class="" ga-label="Kei Nishikori" ga-action="" ga-category="" ga-use="true">
+                            Kei Nishikori</a>
+re.sub(r"^\s+|\s+$", "", s)
+
+rank-cell
+move-cell
+player-cell
+age-cell
+points-cell
+tourn-cell
+pts-cell
+next-cell
+
 rankDateList = []
 rankDateList = soup.find_all('li', text = 'date-value')
 print(len(rankDateList))
